3998d12e7354e6e12dac5e848c94963b4281bc25
w1dok <68337973+w1dok@users.noreply.github.com>
2020-10-31 21:08:21 +0300

    Add files via upload

---
+++ test/core/src/com/mygdx/game/Bird.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Bird {
+    Texture img;
+    Vector2 position;
+    float vy;
+    float gravity;
+
+    public  Bird () {
+        img = new Texture("bird1.png");
+        position = new Vector2(100, 380);
+        vy = 0;
+        gravity = - 0.5f;
+    }
+    public void render (SpriteBatch batch) {
+        batch.draw(img, position.x, position.y);
+    }
+
+    public void update () {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            vy = 5;
+        }
+        vy += gravity;
+        position.y += vy;
+    }
+
+    public  void recreate () {
+        position = new Vector2(100, 380);
+        vy = 0;
+    }
+}
